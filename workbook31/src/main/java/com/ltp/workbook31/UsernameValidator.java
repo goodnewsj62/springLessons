1c4e31f4e0312635afbfb199e9825303c4e32143
goodnews <goodnewsj62@gmail.com>
2023-10-02 13:18:38 +0100

    field validation

---
+++ workbook31/src/main/java/com/ltp/workbook31/UsernameValidator.java
@@ -0,0 +1,18 @@
+package com.ltp.workbook31;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UsernameValidator implements ConstraintValidator<Username,  String>  {
+    
+
+    @Override
+    public boolean isValid(String value,  ConstraintValidatorContext context){
+        Pattern pattern = Pattern.compile("[^a-z0-9 ]");
+        Matcher matcher =  pattern.matcher(value);
+        return matcher.find();
+    }
+}
